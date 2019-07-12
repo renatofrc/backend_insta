a6996b5bbe0f99fd9d0177f6ccbcc79e99f9ba8c
Renato Franco <renatofr95@gmail.com>
2019-07-12 18:01:42 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const multer = require('multer');
+const uploadsConfig = require('./config/upload');
+const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
+
+var routes = new express.Router();
+const upload = multer(uploadsConfig);
+
+
+routes.post('/posts', upload.single('image'), PostController.store);
+routes.get('/posts', PostController.index);
+
+routes.post('/posts/:id/like', LikeController.store);
+
+
+module.exports = routes;
