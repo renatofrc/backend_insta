2010803b059c48bb887ddc9e272973ae6ed57cfb
Renato Franco <renato.franco@eticasolucoes.com.br>
2020-02-09 18:30:56 -0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-mongoose.connect('mongodb+srv://root:aaasao3as@cluster0-knw2t.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true
+mongoose.connect("mongodb://localhost/instagram", {
+  useNewUrlParser: true
 });
 
 app.use((req, res, next) => {
-    req.io = io;
-    next();
+  req.io = io;
+  next();
 });
 
 app.use(cors());
 
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-server.listen(8080)
+server.listen(3333);
